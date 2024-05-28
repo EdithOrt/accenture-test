16e2e902aec9009bdf2a76e6fc04b485c8f81257
EdithOrt <eoc.diseno@gmail.com>
2024-05-28 08:15:44 -0600

    [UPDATE] finish card

--- src/button.js
+++ src/button.js
@@ -8,6 +8,24 @@ class Button extends HTMLElement {
     connectedCallback(){
         this.querySelector("card");
         this.innerHTML = `
+        <style>
+          button {
+              background-color: #7D7D7D;
+              color: white;
+              border: none;
+              padding: 12px 24px;
+              cursor: pointer;
+              border-radius: 50px;
+              font-weight: bold;
+              text-transform: uppercase;
+              width: 100%;
+          }
+
+          button:hover{
+            background-color: black;
+            color: white;
+          }
+        </style>
         <button> 
         Ver más
         </button>
